68c3f1316818099f8b260c007bde0ea299f2ec7b
Anna <kowalewskajaanna@gmail.com>
2023-09-26 23:13:40 +0200

    fix:styles

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 let quoteObject = {};
+getQuotes();
+
 async function getQuotes() {
-    const quotes = 'belarusian_quotes.json';
+    const quotes = 'rusian_quotes.json';
     const res = await fetch(quotes);
     const data = await res.json();
-    const num = Math.round(Math.random() * (data.length - 2));
+    const num = generateNum(data);
     quoteObject = data[num];
-    
+
 }
-getQuotes();
+
 
 
 const quotesText = document.getElementById('quotes__text'),
-    quotesImg = document.getElementById('quotes__img'),
+    // quotesImg = document.getElementById('quotes__img'),
     quotesAuthor = document.getElementById('quotes__author'),
-    quotesButton = document.getElementById('quotes__button');
+    quotesButton = document.getElementById('quotes__button'),
+    body = document.getElementById('body');
+
+const COLORS1 = ['#683611', '#813919', '#96581C', '#D2973E', '#c97b42', '#ae4f0c', '#521808'];
+const COLORS2 = ['#c67815', '#daa628', '#EEC76F', '#f6f1b5', '#F6E8B8', '#ffdbaf', '#f8ebd3'];
 
 quotesButton.addEventListener("click", changeQuotes);
 
-function changeQuotes() {
-    quotesText.innerHTML = quoteObject.text;
-    quotesAuthor.innerHTML =quoteObject.author;
-    getQuotes();
+function generateNum(data) {
+    return Math.round(Math.random() * (data.length - 1));
 }
 
+function changeQuotes() {
+    quotesText.innerHTML = quoteObject.text;
+    quotesAuthor.innerHTML = quoteObject.author;
+    getQuotes();
+    const numColor = generateNum(COLORS1);
+    body.style.cssText = `
+    background-color:${COLORS1[numColor]};
+    color:${COLORS2[numColor]};
+    `;
+    quotesButton.style.cssText = `
+    background-color:${COLORS2[numColor]};
+    color:${COLORS1[numColor]};
+    `
+}
 
-changeQuotes();
+setTimeout(changeQuotes, 200);
+
