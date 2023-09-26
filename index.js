cd69553fc6c532802cbdd3ac87623d033ab302f9
Anna <kowalewskajaanna@gmail.com>
2023-09-26 21:40:38 +0200

    fix:change quotes

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-async function getQuotes() {  
+let quoteObject = {};
+async function getQuotes() {
     const quotes = 'belarusian_quotes.json';
     const res = await fetch(quotes);
-    const data = await res.json(); 
-    console.log(data);
-  }
-  getQuotes();
+    const data = await res.json();
+    const num = Math.round(Math.random() * (data.length - 2));
+    quoteObject = data[num];
+    
+}
+getQuotes();
+
+
+const quotesText = document.getElementById('quotes__text'),
+    quotesImg = document.getElementById('quotes__img'),
+    quotesAuthor = document.getElementById('quotes__author'),
+    quotesButton = document.getElementById('quotes__button');
+
+quotesButton.addEventListener("click", changeQuotes);
+
+function changeQuotes() {
+    quotesText.innerHTML = quoteObject.text;
+    quotesAuthor.innerHTML =quoteObject.author;
+    getQuotes();
+}
+
+
+changeQuotes();
